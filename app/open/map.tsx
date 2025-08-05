b3ebc8dfec416151e1f72331f3bf3b69664906f5
Ro Oeurn <ro@nowopen.studio>
2025-08-05 02:09:46 -0700

    new

--- app/open/map.tsx
+++ app/open/map.tsx
@@ -17,6 +17,7 @@ export default function Map({data}:any){
     const ref = useRef<HTMLDivElement>(null)
     let timer:any = null;
     const [active, setActive] = useState(false)
+      const [info, setInfo] = useState(false)
       const [skrink, setSkrink] = useState(false)
    const [mousePos, setMousePos] = useState({ x: 0.0, y: 0.0 });
     const [pan,setPan]=useState({ x: 0.0, y: 0.0 });
@@ -57,6 +58,13 @@ export default function Map({data}:any){
   
   }
 
+    const toggleInfo=()=>{
+     
+    setInfo(!info)
+
+  
+  }
+
     const trackMouse=(e:any)=>{
         const vx =  ((e.clientX - window.innerWidth/2)/window.innerWidth)
         const vy =  ((e.clientY - window.innerHeight/2)/window.innerHeight)
@@ -66,6 +74,7 @@ export default function Map({data}:any){
     }
 
     const deactivate=()=>{
+      setInfo(false)
       setActive(false)
       setSkrink(false)
           setPan({ x: 0.0, y: 0.0 })
@@ -83,9 +92,18 @@ export default function Map({data}:any){
               {data.length && skrink?(
                 <div className="fixed p-2 top-0 left-0 z-[100] mix-blend-difference w-[100vw] flex justify-between items-center">
                   <Logo onClick={deactivate} className="h-[40px] w-auto"/>
-                  <div><p className="text-[#089ed6] uppercase nav">info</p></div>
+                  <div><p onClick={toggleInfo} className="text-[#089ed6] uppercase nav">info</p></div>
                 </div>
               ):('')}
+
+              
+                  <div onClick={toggleInfo} className={`fixed p-2 top-0 left-0 w-[100vw] h-[100dvh] z-[99] info ${info?'pointer-events-auto active':'pointer-events-none'}`} style={{backgroundColor:`rgba(0,0,0,.9)`}}>
+                      <div className="w-1/2 absolute center-object text-[#089ed6] uppercase nav">
+                      <p className="mb-4 nav">Open Area is live on ‘New Nature’ 24/7 365 days a year.</p>
+<p className="nav">A video and content radio station that has a constant feed of the world’s most fascinating pioneers actively out there in the world discovering and re-writing what we mean when we say Nature. This is a place for people, brands, and mass discovery.</p>
+                      </div>
+                  </div>
+             
             <div className="relative">
                   <div id="fullMap" ref={ref} className="map h-auto flex relative items-center  overflow-hidden z-40 " style={{width:skrink?winX*3:winX*6,scale:skrink?1:.1667,transformOrigin:`0 16.67%`,translate:`${skrink?`${pan.x}px`:'0'} ${skrink?`${pan.y}px`:'0'}`}} >
                     <div onTransitionEnd={active?(()=>setSkrink(true)):(undefined)} onClick={toggle} className={` overflow-hidden h-auto flex-shrink-0 openLogo ${active?'pointer-events-none':""}`} style={{opacity:skrink?0:1,width:winX*3,maxWidth:active?0:10000}} ><OpenLogo fill={skrink?"#EF03AC":"#089ed6"} className={`w-full h-auto`} style={{width:winX*3}}/></div>
